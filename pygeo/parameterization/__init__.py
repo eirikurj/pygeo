3af8fef0e725bbbed60169b1ef5a40b08b5f5ba7
Eirikur Jonsson <eirikurj@umich.edu>
2025-04-17 10:17:23 +0000

    disabling CST import from init

--- pygeo/parameterization/__init__.py
+++ pygeo/parameterization/__init__.py
@@ -1,6 +1,6 @@
 from .DVGeo import DVGeometry
 from .DVGeoAxi import DVGeometryAxi
-from .DVGeoCST import DVGeometryCST
+#from .DVGeoCST import DVGeometryCST
 from .DVGeoVSP import DVGeometryVSP
 from .DVGeoESP import DVGeometryESP
 from .DVGeoMulti import DVGeometryMulti
